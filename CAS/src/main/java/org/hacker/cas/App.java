b9f0a9adb7b984ea8fc075c943ef52a38db22956
Mr.J <chenxj@zjhcsoft.com>
2016-10-20 12:57:02 +0800

    init cas

---
+++ CAS/src/main/java/org/hacker/cas/App.java
@@ -0,0 +1,13 @@
+package org.hacker.cas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
