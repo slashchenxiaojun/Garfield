8665b54b35dfd560c275b8aa8de01fd3fc58e318
Administrator <Administrator@YL-201608031849>
2016-11-16 22:34:04 +0800

    补充DefaultCasService的Demo代码示例

--- Garfield-CAS/src/main/java/org/hacker/cas/sso/DefaultCasService.java
+++ Garfield-CAS/src/main/java/org/hacker/cas/sso/DefaultCasService.java
@@ -6,6 +6,21 @@ public class DefaultCasService extends AbstractCasService{
 
 	@Override
 	protected Object authenticationUser(Credentials credentials) {
+		/**
+		 * 这里可以覆盖API完成对用户的获取
+		 * Demo:
+		 * 
+		 * Object user = User.dao.find("select * from user where username = ? and password = ?")
+		 * if(user == null) return null;
+		 * if(user.password != md5(credentials.password)) return null;
+		 * 
+		 * JSONObject obj = = new JSONObject();
+		 * user.put("id", user.id);
+		 * user.put("username", user.username);
+		 * ...
+		 * 
+		 * return obj;
+		 */
 		JSONObject user = new JSONObject();
 		user.put("username", credentials.username);
 		return user;
